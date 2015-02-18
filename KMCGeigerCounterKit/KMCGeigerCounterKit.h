04be28b75b13b8ec43c08f86a47c6b406dc5629b
Mike Abdullah <me@mikeabdullah.net>
2015-02-18 18:43:46 +0000

    Ability to build a Geiger Counter framework

---
+++ KMCGeigerCounterKit/KMCGeigerCounterKit.h
@@ -0,0 +1,19 @@
+//
+//  KMCGeigerCounterKit.h
+//  KMCGeigerCounterKit
+//
+//  Created by Mike on 18/02/2015.
+//  Copyright (c) 2015 Kevin Conner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KMCGeigerCounterKit.
+FOUNDATION_EXPORT double KMCGeigerCounterKitVersionNumber;
+
+//! Project version string for KMCGeigerCounterKit.
+FOUNDATION_EXPORT const unsigned char KMCGeigerCounterKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KMCGeigerCounterKit/PublicHeader.h>
+
+
